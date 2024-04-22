4a4f4ab037082a8c4bfa45dcd36905b46cee4ae1
Deni <dendi_maksimof@mail.ru>
2024-04-22 17:44:27 +0300

    end3

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,23 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decodeFields(encoded, translations) {
+    return encoded.map(obj => {
+        const decodedObj = {};
+        for (const key in obj) {
+            if (key.endsWith('Id') && !['groupId', 'service', 'formatSize', 'ca'].includes(key)) {
+                const translationKey = obj[key];
+                const translatedValue = translations[translationKey] || ''; 
+                decodedObj[key.slice(0, -2)] = translatedValue; 
+            } else {
+                decodedObj[key] = obj[key];
+            }
+        }
 
+        return decodedObj;
+    });
+}
 
+const decoded = decodeFields(encoded, translations)
 
-// console.log(decoded)
+console.log("Расшифрованные данные:")
+console.log(decoded)
